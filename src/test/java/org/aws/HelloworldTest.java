92c303280bde93ac7fdf5c40339b7f4e31a7ea5f
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-07-24 15:20:19 +0530

    Update HelloworldTest.java

--- src/test/java/org/aws/HelloworldTest.java
+++ src/test/java/org/aws/HelloworldTest.java
@@ -1,4 +1,5 @@
 package org.aws;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,7 +7,7 @@ public class HelloworldTest {
     @Test
     public void handleRequest() {
         Helloworld helloWorld = new Helloworld();
-        String result = helloWorld.handleRequest(null, null);
+        String result = helloWorld.handleRequest(); // Call without arguments
 
         // Update the expected string to "<Hello , AWS lambda>"
         String expected = "<Hello , AWS lambda>";
