f0ded03fef9f8f2d6ccfdc6440a135f369acc2b9
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-07-25 22:20:26 +0530

    Update Helloworld.java

--- src/main/java/org/aws/Helloworld.java
+++ src/main/java/org/aws/Helloworld.java
@@ -1,8 +1,43 @@
 package org.aws;
 
-public class Helloworld {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-    public String handleRequest(){
-        return "Hello AWS Wolrd";
+import java.util.HashMap;
+import java.util.Map;
+
+public class Helloworld implements RequestHandler<Map<String, Object>, Helloworld.ApiGatewayResponse> {
+
+    public Helloworld.ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        String body = "\"Hello AWS World\"";
+
+        return new Helloworld.ApiGatewayResponse(body, headers, 200);
+    }
+
+    public static class ApiGatewayResponse {
+        private final String body;
+        private final Map<String, String> headers;
+        private final int statusCode;
+
+        public ApiGatewayResponse(String body, Map<String, String> headers, int statusCode) {
+            this.body = body;
+            this.headers = headers;
+            this.statusCode = statusCode;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public Map<String, String> getHeaders() {
+            return headers;
+        }
+
+        public int getStatusCode() {
+            return statusCode;
+        }
     }
 }
