4f0215b7f9d285e4f348cdeeff741e1f3fbfebfc
atharva23 <43331286+atharva23@users.noreply.github.com>
2023-07-25 22:21:12 +0530

    Update HelloworldTest.java

--- src/test/java/org/aws/HelloworldTest.java
+++ src/test/java/org/aws/HelloworldTest.java
@@ -1,16 +1,30 @@
 package org.aws;
 
 import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloworldTest {
-    @Test
-    public void handleRequest() {
-        Helloworld helloWorld = new Helloworld();
-        String result = helloWorld.handleRequest(); // Call without arguments
 
-        // Update the expected string to "<Hello , AWS lambda>"
-        String expected = "Hello AWS Wolrd";
-        assertEquals(expected, result);
+    @Test
+    public void testHandleRequest() {
+        Helloworld helloworld = new Helloworld();
+
+        // Create the input for the Lambda function
+        Map<String, Object> input = new HashMap<>();
+
+        // You can add any additional input parameters needed by your Lambda function
+        // For example, if your Lambda function expects a query parameter named "name"
+        // input.put("queryStringParameters", Collections.singletonMap("name", "John"));
+
+        // Invoke the Lambda function
+        Helloworld.ApiGatewayResponse response = helloworld.handleRequest(input, null);
+
+        // Assert the response
+        assertEquals("\"Hello AWS World\"", response.getBody());
+        assertEquals(200, response.getStatusCode());
+        assertEquals("application/json", response.getHeaders().get("Content-Type"));
     }
 }
